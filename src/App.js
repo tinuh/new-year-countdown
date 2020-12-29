f61668080fb50cf62bfaee51a9f37dc03e714d0d
tinuh <tinuvanapamula@gmail.com>
2020-12-29 16:11:54 -0500

    card and styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import ParticlesBg from "particles-bg";
 
-function App() {
+const minuteSeconds = 60;
+const hourSeconds = 3600;
+const daySeconds = 86400;
+
+const timerProps = {
+  isPlaying: true,
+  size: 220,
+  strokeWidth: 10
+};
+
+const renderTime = (dimension, time) => {
   return (
-    <div className="App">
+    <div className="time-wrapper">
+      <div className="time">{time}</div>
+      <div>{dimension}</div>
+    </div>
+  );
+};
+
+const getTimeSeconds = (time) => (minuteSeconds - time / 1000) | 0;
+const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
+const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0;
+const getTimeDays = (time) => (time / daySeconds) | 0;
+
+export default function App() {
+
+  var dateFuture = new Date(new Date().getFullYear() + 1, 0, 1);
+  var dateNow = new Date();
+
+  var seconds = Math.floor((dateFuture - dateNow) / 1000);
+
+  const remainingTime = seconds;
+  const days = Math.ceil(remainingTime / daySeconds);
+  const daysDuration = days * daySeconds;
+
+  return (
+    <div className="outer">
       <ParticlesBg color="random" type="tadpole" bg={true}/>
+
+      <div className = "inner">
+        <CountdownCircleTimer
+          {...timerProps}
+          colors={[["#7E2E84"]]}
+          duration={daysDuration}
+          initialRemainingTime={remainingTime}
+        >
+          {({ elapsedTime }) =>
+            renderTime("days", getTimeDays(daysDuration - elapsedTime / 1000))
+          }
+        </CountdownCircleTimer>
+        <CountdownCircleTimer
+          {...timerProps}
+          colors={[["#D14081"]]}
+          duration={daySeconds}
+          initialRemainingTime={remainingTime % daySeconds}
+          onComplete={(totalElapsedTime) => [
+            remainingTime - totalElapsedTime > hourSeconds
+          ]}
+        >
+          {({ elapsedTime }) =>
+            renderTime("hours", getTimeHours(daySeconds - elapsedTime / 1000))
+          }
+        </CountdownCircleTimer>
+        <CountdownCircleTimer
+          {...timerProps}
+          colors={[["#EF798A"]]}
+          duration={hourSeconds}
+          initialRemainingTime={remainingTime % hourSeconds}
+          onComplete={(totalElapsedTime) => [
+            remainingTime - totalElapsedTime > minuteSeconds
+          ]}
+        >
+          {({ elapsedTime }) =>
+            renderTime(
+              "minutes",
+              getTimeMinutes(hourSeconds - elapsedTime / 1000)
+            )
+          }
+        </CountdownCircleTimer>
+        <CountdownCircleTimer
+          {...timerProps}
+          colors={[["#218380"]]}
+          duration={minuteSeconds}
+          initialRemainingTime={remainingTime % minuteSeconds}
+          onComplete={(totalElapsedTime) => [
+            remainingTime - totalElapsedTime > 0
+          ]}
+        >
+          {({ elapsedTime }) =>
+            renderTime("seconds", getTimeSeconds(elapsedTime))
+          }
+        </CountdownCircleTimer>
+      </div>
     </div>
   );
 }
-
-export default App;
